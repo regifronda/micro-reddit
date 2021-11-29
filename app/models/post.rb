3c8faedd17d6633da9ab5f6e8cc27c2895e72c8b
Regi Fronda <regisfronda@gmail.com>
2021-11-29 15:30:05 -0800

    Add commenting such that each comment refers to a post and the user who has authored it

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
   validates :title, presence: true, uniqueness: true, length: { maximum: 100 }
   validates :body, presence: true, length: { maximum: 40000 }
 end
